c27c554aba1de3df0af4b61fe8658de2b7cbc370
aliserv <44829784+aliserv@users.noreply.github.com>
2018-11-26 16:27:52 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,87 @@
+const Discord = require('discord.js');
+
+const fs = require('fs')
+
+const client = new Discord.Client();
+
+                    
+
+const prefix = '';
+
+client.on('ready', () => {
+
+  
+    console.log(`Logged in as [ Foil]`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if (message.content === 'مساء الخير') {
+          let count = 1;
+          let ecount = 1;
+          for(let x = 1; x < 99999; x++) {
+            message.channel.send(`**@£$;÷($,×(,$(÷,$(÷,/¥÷,/¥÷,%(÷()÷¥$€$÷*;$(÷,÷(÷,×(÷,×¥$;÷¥×;($(÷,$(÷,$¥=,(×,$(÷,$¥÷,¥$,₩:,×(/×(,$¥×,/(÷($,÷¥/÷(,/(;/(÷,₩$₩2¥2₩$*#("×,)×,÷(/,÷¥÷)$,(×,$¥×(÷,÷¥/?÷(€÷((=×,(/*÷¥÷(?(×(=?)÷,$¥÷&$@Nh,#0705  ÷, `)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
